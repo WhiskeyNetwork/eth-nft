a449376522091ae7d21684a12a56c331282f53e6
Matt <38109405+mmcguire91@users.noreply.github.com>
2022-06-25 14:12:19 -0500

    fixed rendering issues

--- contracts/WhiskeyNFT.sol
+++ contracts/WhiskeyNFT.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.1;
 
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
@@ -31,20 +32,20 @@ contract WhiskeyNFT is ERC721URIStorage {
     //Function to randomly pick a word from each array
     function pickRandomFirstWord(uint256 tokenId) public view returns (string memory) {
         //seed the random generator
-        uint rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
+        uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
         //Squash the # between 0 and the length of the array to avoid going out of bounds
         rand = rand % firstWords.length;
         return firstWords[rand];
     }
 
     function pickRandomSecondWord(uint256 tokenId) public view returns (string memory) {
-        uint rand = random(string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId))));
+        uint256 rand = random(string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId))));
         rand = rand % secondWords.length;
         return secondWords[rand];
     }
 
     function pickRandomThirdWord(uint256 tokenId) public view returns (string memory) {
-        uint rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId))));
+        uint256 rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId))));
         rand = rand % thirdWords.length;
         return thirdWords[rand];
     }
@@ -65,7 +66,7 @@ contract WhiskeyNFT is ERC721URIStorage {
         string memory combinedWord = string(abi.encodePacked(first, second, third));
 
         //concatenate it all together and close the <text> and <svg> tags
-        string memory finalSvg = string(abi.encodePacked(baseSvg, first, second, third, "</text></svg>"));
+        string memory finalSvg = string(abi.encodePacked(baseSvg, combinedWord, "</text></svg>"));
 
         //format and map the JSON metadata and base64 encode it
         string memory json = Base64.encode(
@@ -75,7 +76,7 @@ contract WhiskeyNFT is ERC721URIStorage {
                         '{"name": "',
                         //set the title of the nft to that of the randomly generated word
                         combinedWord,
-                        '", "description": "It\'s hip to be square", "image": "data:image/svg+xml;base64',
+                        '", "description": "It\'s hip to be square", "image": "data:image/svg+xml;base64,',
                         //Add data:image/svg+xml;base64 and then append our base64 encode our svg
                         Base64.encode(bytes(finalSvg)),
                         '"}'
